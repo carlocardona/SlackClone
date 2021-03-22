3fdd2eb6e338871167251543182c192e2d79f126
Carlo <carlo.dev2@gmail.com>
2021-03-22 14:08:16 +1100

    added firbase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5tNpiI1xs4r8RV_oOqBRN785FVRIMQcI",
+    authDomain: "slackclone-cdaef.firebaseapp.com",
+    projectId: "slackclone-cdaef",
+    storageBucket: "slackclone-cdaef.appspot.com",
+    messagingSenderId: "97881629616",
+    appId: "1:97881629616:web:694c9ff9bbef34788fec75"
+  };
