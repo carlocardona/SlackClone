652a63b9d4e8033e2f113fa04a1c1a19a189fb28
Carlo <carlo.dev2@gmail.com>
2021-03-23 16:10:38 +1100

    adding header using styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <div className="app">
-      <h1>SlackCLone</h1>
+      <Router>
+            <>
+              <Switch>
+                <Route path="/" exact>
+                  <Header />
+                </Route>
+              </Switch>
+            </>
+      </Router>    
     </div>
   );
 }
