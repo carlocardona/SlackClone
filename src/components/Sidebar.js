70c2d18cfad5c0777e039cd7befcedb4a80cbb6b
Carlo <carlo.dev2@gmail.com>
2021-03-25 23:07:08 +1100

    fixed styling for create button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import CreateIcon from '@material-ui/icons/Create'
 
 function Sidebar() {
     return (
@@ -8,7 +9,12 @@ function Sidebar() {
            <SidebarHeader>
                 <SidebarInfo>
                     <h2>Slack Clone</h2>
+                    <h3>
+                        <FiberManualRecordIcon />
+                        Elon Musk
+                    </h3>
                 </SidebarInfo>
+                <CreateIcon />
            </SidebarHeader>
        </SidebarContainer>
     )
@@ -17,11 +23,26 @@ function Sidebar() {
 export default Sidebar
 
 const SidebarContainer = styled.div`
-
+    color: white;
+    background-color: var(--slack-color);
+    flex: 0.3;
+    border-top: 1px solid #49274b;
+    max-width: 260px;
+    margin-top: 60px;
 `;
 
 const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
 
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 999px;
+    }
 `;
 
 const SidebarInfo = styled.div`
