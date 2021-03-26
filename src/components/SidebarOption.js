7ef014850b66ade066517fcd36c804143adb7089
Carlo <carlo.dev2@gmail.com>
2021-03-26 20:40:02 +1100

    starting sidebar options

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SidebarOption() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SidebarOption
+
+const SidebarOptionContainer = styled.div ``;
