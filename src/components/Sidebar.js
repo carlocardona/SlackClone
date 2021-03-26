de6ffb685157717878c84874fe8d4aa9dc5bd7dd
Carlo <carlo.dev2@gmail.com>
2021-03-26 22:55:26 +1100

    making the actions to add channel

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import CreateIcon from '@material-ui/icons/Create'
+import SidebarOption from './SidebarOption'
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
 
 function Sidebar() {
     return (
@@ -16,6 +27,22 @@ function Sidebar() {
                 </SidebarInfo>
                 <CreateIcon />
            </SidebarHeader>
+
+           <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
+           <SidebarOption Icon={InboxIcon} title="Mentions & Reactions"/>
+           <SidebarOption Icon={DraftsIcon} title="Saved Items"/>
+           <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser"/>
+           <SidebarOption Icon={PeopleAltIcon} title="People"/>
+           <SidebarOption Icon={AppsIcon} title="Apps"/>
+           <SidebarOption Icon={FileCopyIcon} title="File Browser"/>
+           <SidebarOption Icon={ExpandLessIcon} title="Show Less"/>
+           <hr/>
+           <SidebarOption Icon={ExpandMoreIcon} title="Channel"/>
+           <hr/>
+           <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel"/>
+
+
+
        </SidebarContainer>
     )
 }
@@ -29,6 +56,12 @@ const SidebarContainer = styled.div`
     border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b;
+    }
 `;
 
 const SidebarHeader = styled.div`
