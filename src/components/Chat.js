3488dbd85c0e724fb5f4d4e5ac22b8d40f51129b
Carlo <carlo.dev2@gmail.com>
2021-03-28 09:53:53 +1100

    finished chat header styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,23 @@
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import React from 'react'
 import styled from 'styled-components'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
 
 function Chat() {
     return (
        
         <ChatContainer>
-            <h1>Hello</h1>
             <Header>
-
+                <HeaderLeft>
+                    <h4><strong>#Room Name</strong></h4>
+                    <StarBorderOutlinedIcon />
+                </HeaderLeft>
+                <HeaderRight>
+                    <p>
+                        <InfoOutlinedIcon /> Details
+                    </p>
+                </HeaderRight>
             </Header>
         </ChatContainer>    
         
@@ -22,9 +31,42 @@ const ChatContainer = styled.div`
     flex-grow: 1;
     overflow-y: scroll;
     margin-top: 60px;
-    padding-left: 10px;
 `;
 
 const Header = styled.div`
-
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 1px solid lightgray;
 `;
+
+const HeaderLeft = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    > h4 {
+        display: flex;
+        text-transform: lowercase;
+        margin-right: 10px;
+    }
+
+    > h4 > .MuiSvgIcon-root {
+        margin-left: 10px;
+        font-size: 18px;
+    }
+`;
+
+const HeaderRight = styled.div`
+    > p {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+    }
+
+    > p > .MuiSvgIcon-root {
+        margin-right: 5px !important;
+        font-size: 16px;
+    }
+`;
+
