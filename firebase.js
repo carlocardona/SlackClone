db4b01a1cd4c92c78eba22bfab56839043f726e3
Carlo <carlo.dev2@gmail.com>
2021-03-27 19:47:33 +1100

    connecting firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyC5tNpiI1xs4r8RV_oOqBRN785FVRIMQcI",
     authDomain: "slackclone-cdaef.firebaseapp.com",
@@ -6,3 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "97881629616",
     appId: "1:97881629616:web:694c9ff9bbef34788fec75"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
